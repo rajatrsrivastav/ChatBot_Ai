444159f5e2b3470c172ce351a310efc5a83f815b
Rajat Srivastav <ratneshrsrivastav@gmail.com>
2025-04-27 22:43:11 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,7 @@
 import { fileURLToPath } from "url";
+import db from 'src/db';  // Default export from src/db/index.js
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = __filename.substring(0, __filename.lastIndexOf("/"));
+
 export default __dirname;
